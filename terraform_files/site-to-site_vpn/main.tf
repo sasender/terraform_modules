20655055552d28f47ab1c26164e67b9156af02b7
sasender <sasender123@gmail.com>
2023-11-09 16:01:32 +0530

    adding the data resourse

--- terraform_files/site-to-site_vpn/main.tf
+++ terraform_files/site-to-site_vpn/main.tf
@@ -94,3 +94,8 @@ static_routes_only = true # Set to true for static routes
 
 
 }
+
+data "aws_ec2_transit_gateway_vpn_attachment" "example" {
+transit_gateway_id = aws_ec2_transit_gateway.example.id
+vpn_connection_id = aws_vpn_connection.example.id
+}
